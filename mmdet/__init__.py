915263b1cdd918fba176ceacc8837124807ac40d
enurach <nurali.cherniyazov03@gmail.com>
2024-05-10 03:02:48 +0500

    finish

---
+++ mmdet/__init__.py
@@ -0,0 +1,23 @@
+import mmcv
+
+from .version import __version__, short_version
+
+
+def digit_version(version_str):
+    digit_version = []
+    for x in version_str.split('.'):
+        if x.isdigit():
+            digit_version.append(int(x))
+        elif x.find('rc') != -1:
+            patch_version = x.split('rc')
+            digit_version.append(int(patch_version[0]) - 1)
+            digit_version.append(int(patch_version[1]))
+    return digit_version
+
+
+mmcv_minimum_version = '1.1.5'
+mmcv_maximum_version = '1.4'
+mmcv_version = digit_version(mmcv.__version__)
+
+
+
